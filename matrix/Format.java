3de5286923b5d1c54b6685b77e7b1556c95e6405
sonicair <ishitah.takeshi@gmail.com>
2015-12-01 22:01:37 +0900

    Add a method to print ordinal numbers

---
+++ matrix/Format.java
@@ -0,0 +1,18 @@
+package matrix;
+
+
+class Format {
+    //exhibit given number using the English ordinal suffix
+    public static String ordinal(int n) {
+        if(n == 1) {
+            return "1st";
+        }
+        if(n == 2) {
+            return "2nd";
+        }
+        if(n == 3) {
+            return "3rd";
+        }
+        return n + "th";
+    }
+}
